fa96da9ef2754d791d32f7d8850dc40f207de533
MHEN007 <91794056+MHEN007@users.noreply.github.com>
2025-03-23 00:03:23 +0700

    feat: CORS

--- backend/src/controller/auth.controller.ts
+++ backend/src/controller/auth.controller.ts
@@ -3,15 +3,11 @@ import Elysia from "elysia";
 export const AuthController = new Elysia()
     .post(
         '/login',
-        async ({ body }: { body: {
-            username: string;
-            password: string;
-        }}) => {
-            const { username, password } = body;
-            if (username === 'admin' && password === 'admin') {
-                return { token: 'admin' };
+        async ({ body } : { body: {username: string; password: string; } }) => {
+            if (body.username === 'admin' && body.password === 'admin') {
+                return { token: 'admin', username: 'admin' };
             } else {
                 return { error: 'Invalid username or password' };
             }
-        }
+        },
     )
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,17 @@
 import { Elysia } from "elysia";
 import { Routes } from "./route";
+import { cors } from "@elysiajs/cors";
 
 const app = new Elysia()
   .get("/", () => "Hello Elysia")
   .use(Routes)
+  .use(cors(
+    {
+      origin: "*",
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+    }
+  ))
   .listen(3001);
 
 console.log(
